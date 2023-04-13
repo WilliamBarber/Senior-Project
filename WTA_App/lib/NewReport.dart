4b7206c4fa7a44f405d14ff86046597c8e3ffae1
TslaRoadster2 <wobarber@outlook.com>
2023-04-12 18:02:58 -0700

    Photo Integration Improvements
    Seems to work in emulator for now. Added authentication, uploads image to Firebase, grabs URL, stores that in FireStore

--- WTA_App/lib/NewReport.dart
+++ WTA_App/lib/NewReport.dart
@@ -3,31 +3,36 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class NewReport{
     static void submitReport(String title, String description, double severity, String issue, File? imageFile) async {
-    Firebase.initializeApp();
+      print('');
+      print('');
+      print('');
+      print("attempting submission");
+      print('');
+      print('');
+
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
     var db = FirebaseFirestore.instance;
-    // Create a new report with Description, Title, Severity, Issue type, and Images
-    /*final firebaseImageFile = FirebaseStorage.instance.ref();
-    final reportImage = firebaseImageFile
-        .child("images/reports/reportimage.jpg")
-        .putFile(imageFile!);
-    print('Image File:');
-    print('');
-    print('');
-    print(imageFile);
-    print(reportImage);*/
+
+    //Start Image Submission
+    await FirebaseAuth.instance.signInAnonymously();
+    final storageRef = FirebaseStorage.instance.ref();
+    await storageRef.child("Image File").putFile(imageFile!);
+    final imageURL = await storageRef.child("Image File").getDownloadURL();
+    //End Image Submission
+
+    // Create a new report with Description, Title, Severity, Issue type, and Image URL
     final report = <String, dynamic>{
       "title": title,
       "description": description,
       "severity": severity,
       "issue": issue,
-      //"image": imageFile,
-      //"image": reportImage,
+      "image": imageURL,
     };
 
     // Add a new document with a generated ID
