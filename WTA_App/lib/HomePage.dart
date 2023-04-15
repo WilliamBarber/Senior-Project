fb49186c7f55fa69432f7d7e35ab49b5eb6feb0a
TslaRoadster2 <wobarber@outlook.com>
2023-04-15 16:48:08 -0700

    Added a default message on the homescreen
    The homescreen now shows a default message when there is no new report to submit. I also added a title to the homepage to explain that the reports are, in fact, previous reports.

--- WTA_App/lib/HomePage.dart
+++ WTA_App/lib/HomePage.dart
@@ -8,47 +8,85 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var oldReports = appState.getAllReports();
-    return Center(
-      child: FractionallySizedBox(
-        widthFactor: 0.85,
-        child: ListView.builder(
-          reverse: true,
-            shrinkWrap: true,
-            itemCount: oldReports.length > 3 ? 3 : oldReports.length,
-            itemBuilder: (context, reportNumber) {
-              return Card(
-                margin: EdgeInsets.only(top: 10, bottom: 10),
-                child: FractionallySizedBox(
-                  widthFactor: 0.85,
-                  child: Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                              'Title: ${appState.getReport(reportNumber).getTitle()}'),
-                          Text(
-                              'Date: ${appState.getReport(reportNumber).getDate()}'),
-                          Text(
-                              'Issue Category: ${appState.getReport(reportNumber).getCategory()}'),
-                          ElevatedButton(
-                            child: Text('View Report'),
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => OldReportPage(
-                                        report:
-                                            appState.getReport(reportNumber))),
-                              );
-                            },
-                          ),
-                        ]),
-                  ),
-                ),
-              );
-            }),
+    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Center(
+        child: Text(
+          'Previous Reports:',
+          style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0),
+        ),
       ),
-    );
+      if (oldReports.isEmpty)
+        Center(
+          child: Card(
+            margin: EdgeInsets.only(top: 10, bottom: 10),
+            child: FractionallySizedBox(
+              widthFactor: 0.85,
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text.rich(
+                          TextSpan(
+                              text: 'No reports submitted yet. Click the ',
+                              children: <TextSpan>[
+                                TextSpan(
+                                    text: 'New Report ',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold)),
+                                TextSpan(
+                                    text:
+                                        ' button at the bottom of the screen to begin!'),
+                              ]),
+                          textAlign: TextAlign.center),
+                    ]),
+              ),
+            ),
+          ),
+        )
+      else
+        Center(
+          child: FractionallySizedBox(
+            widthFactor: 0.85,
+            child: ListView.builder(
+                reverse: true,
+                shrinkWrap: true,
+                itemCount: oldReports.length > 3 ? 3 : oldReports.length,
+                itemBuilder: (context, reportNumber) {
+                  return Card(
+                    margin: EdgeInsets.only(top: 10, bottom: 10),
+                    child: FractionallySizedBox(
+                      widthFactor: 0.85,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                  'Title: ${appState.getReport(reportNumber).getTitle()}'),
+                              Text(
+                                  'Date: ${appState.getReport(reportNumber).getDate()}'),
+                              Text(
+                                  'Issue Category: ${appState.getReport(reportNumber).getCategory()}'),
+                              ElevatedButton(
+                                child: Text('View Report'),
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => OldReportPage(
+                                            report: appState
+                                                .getReport(reportNumber))),
+                                  );
+                                },
+                              ),
+                            ]),
+                      ),
+                    ),
+                  );
+                }),
+          ),
+        ),
+    ]);
   }
 }
