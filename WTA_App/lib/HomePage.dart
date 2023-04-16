2059de50c356ac28e1d57bdcd509c95a0a78d1d0
TslaRoadster2 <wobarber@outlook.com>
2023-04-15 18:48:44 -0700

    Added back Homescreen title
    Adde back the homescreen label. It's a pretty dodgy way of doing it, but it seems to work so we're going with it.

--- WTA_App/lib/HomePage.dart
+++ WTA_App/lib/HomePage.dart
@@ -10,31 +10,47 @@ class HomePage extends StatelessWidget {
     var oldReports = appState.getAllReports();
     if (oldReports.isEmpty) {
       return Center(
-        child: Card(
-          margin: EdgeInsets.only(top: 10, bottom: 10),
-          child: FractionallySizedBox(
-            widthFactor: 0.85,
-            child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text.rich(
-                        TextSpan(
-                            text: 'No reports submitted yet. Click the ',
-                            children: <TextSpan>[
-                              TextSpan(
-                                  text: 'New Report ',
-                                  style:
-                                      TextStyle(fontWeight: FontWeight.bold)),
-                              TextSpan(
-                                  text:
-                                      ' button at the bottom of the screen to begin!'),
+        child: FractionallySizedBox(
+          widthFactor: 0.85,
+          child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: 2,
+              itemBuilder: (context, index) {
+                if (index == 0){
+                  return Text(
+                    'Recent Reports',
+                    style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0),
+                  );
+                }
+                else {
+                  return Card(
+                    margin: EdgeInsets.only(top: 10, bottom: 10),
+                    child: FractionallySizedBox(
+                      widthFactor: 0.85,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text.rich(
+                                  TextSpan(
+                                      text: 'No reports submitted yet. Click the ',
+                                      children: <TextSpan>[
+                                        TextSpan(
+                                            text: 'New Report ',
+                                            style:
+                                            TextStyle(fontWeight: FontWeight.bold)),
+                                        TextSpan(
+                                            text:
+                                            'button at the bottom of the screen to begin!'),
+                                      ]),
+                                  textAlign: TextAlign.center),
                             ]),
-                        textAlign: TextAlign.center),
-                  ]),
-            ),
-          ),
+                      ),
+                    ),
+                  );
+                }
+              }),
         ),
       );
     } else {
@@ -43,39 +59,51 @@ class HomePage extends StatelessWidget {
           widthFactor: 0.85,
           child: ListView.builder(
               shrinkWrap: true,
-              itemCount: oldReports.length > 3 ? 3 : oldReports.length,
+              itemCount: oldReports.length > 3 ? 4 : oldReports.length + 1,
               itemBuilder: (context, reportNumber) {
-                return Card(
-                  margin: EdgeInsets.only(top: 10, bottom: 10),
-                  child: FractionallySizedBox(
-                    widthFactor: 0.85,
-                    child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                                'Title: ${appState.getReport(reportNumber).getTitle()}'),
-                            Text(
-                                'Date: ${appState.getReport(reportNumber).getDate()}'),
-                            Text(
-                                'Issue Category: ${appState.getReport(reportNumber).getCategory()}'),
-                            ElevatedButton(
-                              child: Text('View Report'),
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => OldReportPage(
-                                          report: appState
-                                              .getReport(reportNumber))),
-                                );
-                              },
-                            ),
-                          ]),
+                if (reportNumber == 0){
+                  return Text(
+                    'Recent Reports',
+                    style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0),
+                  );
+                }
+                else {
+                  return Card(
+                    margin: EdgeInsets.only(top: 10, bottom: 10),
+                    child: FractionallySizedBox(
+                      widthFactor: 0.85,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                  'Title: ${appState.getReport(reportNumber)
+                                      .getTitle()}'),
+                              Text(
+                                  'Date: ${appState.getReport(reportNumber)
+                                      .getDate()}'),
+                              Text(
+                                  'Issue Category: ${appState.getReport(
+                                      reportNumber).getCategory()}'),
+                              ElevatedButton(
+                                child: Text('View Report'),
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            OldReportPage(
+                                                report: appState
+                                                    .getReport(reportNumber))),
+                                  );
+                                },
+                              ),
+                            ]),
+                      ),
                     ),
-                  ),
-                );
+                  );
+                }
               }),
         ),
       );
