d73cbb825d702354e36156108e4434cfdab0b9f4
TslaRoadster2 <wobarber@outlook.com>
2023-04-16 12:42:09 -0700

    Added an AppBar to prev. reports page
    Allows for easier integration of the "return to previous page" button

--- WTA_App/lib/AllPreviousReportsPage.dart
+++ WTA_App/lib/AllPreviousReportsPage.dart
@@ -11,34 +11,30 @@ class AllPreviousReportsPage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
     var oldReports = appState.getAllReports();
     return Scaffold(
-        /*floatingActionButton: FloatingActionButton.extended(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          tooltip: 'PreviousPage',
-          icon: const Icon(Icons.arrow_back),
-          label: const Text('Previous Page'),
-          foregroundColor: Colors.white,
-          backgroundColor: Colors.brown,
-        ),*/
-        bottomSheet: Container(
-          color: Colors.green,
-          height: 78,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                ElevatedButton(
-                  child: const Text('Return to settings page'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                ),
-              ],
-            ),
-          ),
+        appBar: AppBar(
+          title: Text("All Previous Reports"),
+          titleTextStyle: DefaultTextStyle.of(context)
+              .style
+              .apply(fontSizeFactor: 2.0),
         ),
+        // bottomSheet: Container(
+        //   color: Colors.lightGreen[200],
+        //   height: 78,
+        //   child: Center(
+        //     child: Column(
+        //       mainAxisAlignment: MainAxisAlignment.center,
+        //       mainAxisSize: MainAxisSize.min,
+        //       children: <Widget>[
+        //         ElevatedButton(
+        //           child: const Text('Return to settings page'),
+        //           onPressed: () {
+        //             Navigator.pop(context);
+        //           },
+        //         ),
+        //       ],
+        //     ),
+        //   ),
+        // ),
         body: oldReports.isEmpty
             ? Center(
                 child: FractionallySizedBox(
@@ -88,19 +84,11 @@ class AllPreviousReportsPage extends StatelessWidget {
                 child: FractionallySizedBox(
                   widthFactor: 0.85,
                   child: ListView.builder(
-                      padding: EdgeInsets.only(top: 50, bottom: 85),
+                      padding: EdgeInsets.only(top: 20, bottom: 30),
                       shrinkWrap: true,
                       itemCount: oldReports.length + 1,
                       itemBuilder: (context, reportNumber) {
-                        if (reportNumber == 0) {
-                          return Text(
-                            'All Previous Reports',
-                            style: DefaultTextStyle.of(context)
-                                .style
-                                .apply(fontSizeFactor: 2.0),
-                          );
-                        } else {
-                          return Card(
+                        return Card(
                             margin: EdgeInsets.only(top: 10, bottom: 10),
                             child: FractionallySizedBox(
                               widthFactor: 0.85,
@@ -110,11 +98,11 @@ class AllPreviousReportsPage extends StatelessWidget {
                                     mainAxisAlignment: MainAxisAlignment.center,
                                     children: [
                                       Text(
-                                          'Title: ${appState.getReport(reportNumber - 1).getTitle()}'),
+                                          'Title: ${appState.getReport(reportNumber ).getTitle()}'),
                                       Text(
-                                          'Date: ${appState.getReport(reportNumber - 1).getDate()}'),
+                                          'Date: ${appState.getReport(reportNumber).getDate()}'),
                                       Text(
-                                          'Issue Category: ${appState.getReport(reportNumber - 1).getCategory()}'),
+                                          'Issue Category: ${appState.getReport(reportNumber).getCategory()}'),
                                       ElevatedButton(
                                         child: Text('View Report'),
                                         onPressed: () {
@@ -125,8 +113,7 @@ class AllPreviousReportsPage extends StatelessWidget {
                                                     OldReportPage(
                                                         report: appState
                                                             .getReport(
-                                                                reportNumber -
-                                                                    1))),
+                                                                reportNumber))),
                                           );
                                         },
                                       ),
@@ -134,7 +121,6 @@ class AllPreviousReportsPage extends StatelessWidget {
                               ),
                             ),
                           );
-                        }
                       }),
                 ),
               ));
