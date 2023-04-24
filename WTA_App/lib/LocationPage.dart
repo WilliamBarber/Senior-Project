be1f55a89377aadc94e7ca8a7493f31e30f1c000
TslaRoadster2 <wobarber@outlook.com>
2023-04-23 17:21:09 -0700

    Display current coordinates
    Current coordinates are successfully displayed on the "attach current location" screen, although currently it doesn't do anything with them.

--- WTA_App/lib/LocationPage.dart
+++ WTA_App/lib/LocationPage.dart
@@ -1,14 +1,32 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'MyAppState.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+import 'package:location/location.dart';
 
-class LocationPage extends StatelessWidget {
+
+class LocationPage extends StatefulWidget {
   const LocationPage({super.key});
+
+  @override
+  State<LocationPage> createState() => _LocationPageState();
+}
+
+class _LocationPageState extends State<LocationPage> {
+  late Future<LocationData> _location;
+
+  @override
+  void initState(){
+    super.initState();
+        _location = getCurrentLocation();
+  }
 
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+
     //TODO: stop making this hard-coded in
     //TODO: add permissions request (await Permission.locationWhenInUse.request();)
     String ACCESS_TOKEN =
@@ -19,9 +37,47 @@ class LocationPage extends StatelessWidget {
         titleTextStyle:
             DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0),
       ),
-      body: MapWidget(
-          resourceOptions: ResourceOptions(accessToken: ACCESS_TOKEN)
+      body: FutureBuilder<LocationData>(
+        future: _location,
+        builder: (BuildContext context, AsyncSnapshot<LocationData> snapshot) {
+          if (!snapshot.hasData){
+            return Center(
+              child: Text('Loading, please wait...'),
+            );
+          }
+          else{
+            final currentLocation = snapshot.data;
+            return Center(
+              child: ListView(
+                children: [
+                  /*MapWidget(
+                      resourceOptions: ResourceOptions(accessToken: ACCESS_TOKEN),
+                    cameraOptions: CameraOptions(center: Point(coordinates: Position(-80.1263, 25.7845)).toJson(),),
+                  ),*/
+                  Text('Location: ${currentLocation?.latitude}, ${currentLocation?.longitude}'),
+                ],
+              )
+            );
+          }
+        },
       ),
     );
   }
+  Future<LocationData> getCurrentLocation() async {
+    print('************location attempt***************');
+    final location = Location();
+    LocationData currentLocation = await location.getLocation();
+    print('************location get***************');
+    location.onLocationChanged.listen((LocationData currentLocation) {
+      print("${currentLocation.longitude} : ${currentLocation.longitude}");
+      setState(() {
+        _location = location.getLocation();
+      });
+    });
+    return currentLocation;
+  }
 }
+
+
+
+
